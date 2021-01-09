5d0452a6269ce732d7ea719c06988f93d5b1e733
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-09 22:53:20 +0100

    display applicant info

--- src/components/ApplicantCard.tsx
+++ src/components/ApplicantCard.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import styles from './ApplicantCard.module.css';
+
 export interface Applicant {
   id: number,
   firstName: string,
   lastName: string,
   email: string,
+  phoneNumber: string,
   status: 'Interested' | 'Appointment_Set' | 'Property_Viewed' | 'Offer_Accepted',
   appointmentDate: string | null,
   bid: number | null,
@@ -12,8 +15,25 @@ export interface Applicant {
 
 function ApplicantCard({ applicant }: { applicant: Applicant }): JSX.Element {
   return (
-    <div>
-      { applicant.lastName }
+    <div className={styles.card}>
+      <p>{ `${applicant.firstName} ${applicant.lastName}` }</p>
+      <p>{ applicant.phoneNumber }</p>
+      <p>{ applicant.email }</p>
+      { applicant.appointmentDate && (
+      <p>
+        <span>{ applicant.status === 'Appointment_Set' ? 'Appointment' : 'Viewed' }</span>
+        <span> </span>
+        { applicant.appointmentDate }
+      </p>
+      )}
+      { applicant.bid && (
+      <p>
+        Bid
+        <span> </span>
+        { applicant.bid }
+        <span>€</span>
+      </p>
+      )}
     </div>
   );
 }
