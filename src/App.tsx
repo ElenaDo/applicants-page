7c593103576cf89d2a2d427a180261a80800a9ad
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-09 19:27:52 +0100

    mock data added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import applicants from './data/MOCK_DATA.json';
 
 function App() {
   return (
     <div className="App">
-      test
+      <ul>
+        {
+          applicants.map((applicant) => <li>{applicant.lastName}</li>)
+        }
+      </ul>
     </div>
   );
 }
