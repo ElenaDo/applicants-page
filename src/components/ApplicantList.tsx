749445a603d49d17a5aa2e2144b4f74392d83730
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-10 17:19:28 +0100

    group title style

--- src/components/ApplicantList.tsx
+++ src/components/ApplicantList.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ApplicantCard from './ApplicantCard';
 
+import styles from './ApplicantList.module.css';
+
 function ApplicantList({ applicants }: { applicants: Applicant[] }) {
   const grouped = applicants.reduce((acc, current) => {
     acc[current.status].push(current);
@@ -12,12 +14,15 @@ function ApplicantList({ applicants }: { applicants: Applicant[] }) {
     Interested: [] as Applicant[],
     Offer_Accepted: [] as Applicant[],
   });
+  function groupTitle(title: string) {
+    return title.replace('_', ' ');
+  }
   return (
     <div>
       {
         Object.keys(grouped).map((groupName) => (
           <div key={groupName}>
-            <h3>{groupName}</h3>
+            <h3 className={styles.groupTitle}>{groupTitle(groupName)}</h3>
             {
               grouped[groupName as StatusTypes].map((applicant: Applicant) => (
                 <ApplicantCard applicant={applicant as Applicant} key={applicant.id} />
