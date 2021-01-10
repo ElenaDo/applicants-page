c0648e08ec40096e4630f48d9bf3ad9804efb977
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-10 22:27:46 +0100

    main css updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import './App.css';
 import applicants from './data/MOCK_DATA.json';
 import ApplicantList from './components/ApplicantList';
 import Filters from './components/Filters';
+import backIcon from './assets/back.svg';
 
 function App() {
   return (
     <div className="App">
+      <span className="display-flex">
+        <button type="button" className="button-link back-icon">
+          <img src={backIcon} alt="back" className="" />
+        </button>
+        <h2 className="display-inline-block">Applicants</h2>
+      </span>
       <Filters />
       <ApplicantList applicants={applicants as Applicant[]} />
     </div>
