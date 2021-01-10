f03fbe4f8fb96f79b7ed05dd1cdb5c3a19885a4a
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-10 16:56:52 +0100

    type definitions updates, applicant groups

--- src/components/ApplicantList.tsx
+++ src/components/ApplicantList.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import ApplicantCard from './ApplicantCard';
 
 function ApplicantList({ applicants }: { applicants: Applicant[] }) {
+  const grouped = applicants.reduce((acc, current) => {
+    acc[current.status].push(current);
+    return acc;
+  }, {
+    // explicitly set object keys to preserve order
+    Appointment_Set: [] as Applicant[],
+    Property_Viewed: [] as Applicant[],
+    Interested: [] as Applicant[],
+    Offer_Accepted: [] as Applicant[],
+  });
   return (
     <div>
       {
-        applicants.map((applicant) => (
-          <ApplicantCard applicant={applicant as Applicant} key={applicant.id} />))
+        Object.keys(grouped).map((groupName) => (
+          <div key={groupName}>
+            <h3>{groupName}</h3>
+            {
+              grouped[groupName as StatusTypes].map((applicant: Applicant) => (
+                <ApplicantCard applicant={applicant as Applicant} key={applicant.id} />
+              ))
+            }
+          </div>
+        ))
       }
     </div>
   );
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,16 @@
+type StatusTypes = 'Interested' | 'Appointment_Set' | 'Property_Viewed' | 'Offer_Accepted';
+
 interface Applicant {
   id: number,
   firstName: string,
   lastName: string,
   email: string,
   phoneNumber: string,
-  status: 'Interested' | 'Appointment_Set' | 'Property_Viewed' | 'Offer_Accepted',
+  status: StatusTypes,
   appointmentDate: string | null,
   bid: number | null,
+}
+
+interface ApplicantGroups {
+  [index: StatusTypes]: Applicant[]
 }
