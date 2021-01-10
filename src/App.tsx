28e3be89427df832835cc0c8cb7e2066c861b970
Elena Raspopova <61025167+ElenaDo@users.noreply.github.com>
2021-01-11 00:00:20 +0100

    global and App css separated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import './App.css';
+import './styles.css';
+import styles from './App.module.css';
 import applicants from './data/MOCK_DATA.json';
 import ApplicantList from './components/ApplicantList';
 import Filters from './components/Filters';
+import Stats from './components/Stats';
 import backIcon from './assets/back.svg';
 
 function App() {
   return (
     <div className="App">
-      <span className="display-flex">
-        <button type="button" className="button-link back-icon">
-          <img src={backIcon} alt="back" className="" />
-        </button>
-        <h2 className="display-inline-block">Applicants</h2>
-      </span>
+      <div className={styles.upperPanel}>
+        <span className="display-flex">
+          <button type="button" className={`${styles.buttonLink} ${styles.backIcon}`}>
+            <img src={backIcon} alt="back" className="" />
+          </button>
+          <h2 className="display-inline-block">Applicants</h2>
+        </span>
+        <Stats applicants={applicants as Applicant[]} />
+      </div>
       <Filters />
       <ApplicantList applicants={applicants as Applicant[]} />
     </div>
